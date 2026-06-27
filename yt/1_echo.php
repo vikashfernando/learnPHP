3982a6099f24b511c3d9aa642d6a9d0299765697
vikashfernando <vikashdeshitha@gmail.com>
2026-06-27 13:43:14 +0530

    echo done

---
+++ yt/1_echo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>hellow world</title>
+    </head>
+    <body>
+        <?php
+            echo "hello world"
+        ?>
+
+    </body>
+</html>
+
