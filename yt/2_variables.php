bcffff6456bd085ff9c5ad30e75ae70975ae9c31
vikashfernando <vikashdeshitha@gmail.com>
2026-06-27 13:49:16 +0530

    l2 variables created

---
+++ yt/2_variables.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>php2</title>
+    </head>
+    <body>
+        <?php
+
+            echo "hello world ";
+            $name="vikash";
+            echo "my name is ", $name;
+        ?>
+
+    </body>
+</html>
+
