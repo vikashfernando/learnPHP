30bd7826e923689d9bd0b73679c0c5d8f4467096
vikashfernando <vikashdeshitha@gmail.com>
2026-06-27 15:30:41 +0530

    create comments

---
+++ yt/4_comment.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>hellow world</title>
+    </head>
+    <body>
+        <?php
+            echo "hello world" //this is a PHP single line comment
+
+            /*
+            multiline comment
+
+            echo "hello world"
+            echo "hello world"
+            echo "hello world"
+            echo "hello world"
+            echo "hello world"
+            */
+
+
+        ?>
+
+    </body>
+</html>
+
