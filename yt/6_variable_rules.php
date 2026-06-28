d791444cb1c5943b227ce1e36464e5dd4c6672f4
vikashfernando <vikashdeshitha@gmail.com>
2026-06-28 07:05:03 +0530

    variable rules added L6

---
+++ yt/6_variable_rules.php
@@ -0,0 +1,23 @@
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>hellow world</title>
+    </head>
+    <body>
+        <?php
+            echo "hello world"
+
+            /*
+            Variables cannot start with a number
+                correct - $name,$student,$studentName,$_name$totalMarks
+                wrong - $123name
+
+
+            */
+
+        ?>
+
+    </body>
+</html>
