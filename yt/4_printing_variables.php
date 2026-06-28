2109f3ead613a505fd1582babef83da1352e6dbb
vikashfernando <vikashdeshitha@gmail.com>
2026-06-28 06:48:40 +0530

    printing variables done

---
+++ yt/4_printing_variables.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>php2</title>
+    </head>
+    <body>
+        <?php
+
+            $name="vikash";
+            $age=23
+
+            echo $name;
+            echo $age;
+            
+        ?>
+
+    </body>
+</html>
+
