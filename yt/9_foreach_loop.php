627920f1f4736540c2de6b806d25771ddb9007b6
vikashfernando <vikashdeshitha@gmail.com>
2026-06-28 07:44:05 +0530

    for each done

---
+++ yt/9_foreach_loop.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>hellow world</title>
+    </head>
+    <body>
+        
+        <?php
+            // foreach is used only for arrays
+
+            $colors=array("red","green","blue","white");
+
+            foreach($colors as $color){
+                echo $color."<br>";
+            }
+
+        ?>
+
+    </body>
+</html>
+
