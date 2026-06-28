25396ee162d67b6b16bfea1e58b4af17bbc15bfc
vikashfernando <vikashdeshitha@gmail.com>
2026-06-28 07:26:11 +0530

    done array l7 and indexing

---
+++ yt/7_array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>hellow world</title>
+    </head>
+    <body>
+        
+        <?php
+            //creat array
+            $info=array("vikash","deshitha","fernando",23);
+            
+            //get an element from the array
+            echo "middle name: ".$info[1]."<br>";
+            echo "age: ".$info[3];
+        ?>
+
+    </body>
+</html>
+
