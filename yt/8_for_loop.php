0648d9afaabe8ea81903b7aae07a8ce400fc1011
vikashfernando <vikashdeshitha@gmail.com>
2026-06-28 07:32:42 +0530

    for loop added

---
+++ yt/8_for_loop.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>hellow world</title>
+    </head>
+    <body>
+        
+        <?php
+            
+            /*
+            for(start; condition; increase){
+            code
+            }
+            */
+
+            for($i=1;$i<=5;$i++){
+                echo "hello world <br>";
+            }
+
+        ?>
+
+    </body>
+</html>
+
